ff85077cfd09e154b7fb0664ed011bf777af16eb
anatalibo <antonio.natali@unibo.it>
2019-09-29 12:19:26 +0200

    BasicRobotInteraction ipynb

--- it.unibo.robot2020/basicrobot.pl
+++ it.unibo.robot2020/basicrobot.pl
@@ -1,6 +1,6 @@
 %====================================================================================
 % basicrobot description   
 %====================================================================================
-context(ctxrobot, "localhost",  "TCP", "8020" ).
- qactor( robot, ctxrobot, "it.unibo.robot.Robot").
-  qactor( sonarhandler, ctxrobot, "it.unibo.sonarhandler.Sonarhandler").
+context(ctxbasicrobot, "localhost",  "TCP", "8020" ).
+ qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
+  qactor( sonarhandler, ctxbasicrobot, "it.unibo.sonarhandler.Sonarhandler").
