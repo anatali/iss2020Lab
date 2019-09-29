12f50570bbaed0913d3972cded0f9d0eccf554c4
anatalibo <antonio.natali@unibo.it>
2019-09-29 22:30:30 +0200

    robot2020 redone

--- it.unibo.robot2020/basicrobot.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% basicrobot description   
-%====================================================================================
-context(ctxbasicrobot, "localhost",  "TCP", "8020" ).
- qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
-  qactor( sonarhandler, ctxbasicrobot, "it.unibo.sonarhandler.Sonarhandler").
