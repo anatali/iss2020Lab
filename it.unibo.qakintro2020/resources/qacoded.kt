0bae9074736fb6fa6486cd658e08e9a7a622778e
anatalibo <antonio.natali@unibo.it>
2019-09-30 19:08:47 +0200

    coded and external actors

---
+++ it.unibo.qakintro2020/resources/qacoded.kt
@@ -0,0 +1,14 @@
+package resources
+import it.unibo.kactor.*
+
+class qacoded( name : String ) : ActorBasic( name ){
+ 
+    init{
+		println("	$name starts ")
+    }
+	
+
+    override suspend fun actorBody(msg : ApplMessage){
+        println("	$name handles $msg ")
+    }
+}
