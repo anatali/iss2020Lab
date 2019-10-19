ff0e3c9bdf65e2669d48e7f636780492858b61c0
Antonio Natali <antonio.natali@unibo.it>
2019-10-19 12:37:40 +0200

    smart call python

--- it.unibo.smartrobot2020/smartrobot.pl
+++ it.unibo.smartrobot2020/smartrobot.pl
@@ -1,7 +1,7 @@
 %====================================================================================
 % smartrobot description   
 %====================================================================================
-context(ctxsmartrobot, "localhost",  "TCP", "8020").
-context(ctxbasicrobot, "192.168.1.6",  "TCP", "8018").
- qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( smartrobot, ctxsmartrobot, "it.unibo.smartrobot.Smartrobot").
+context(ctxsmartrobotcaller, "localhost",  "TCP", "8023").
+context(ctxsmartrobot, "192.168.1.4",  "TCP", "8020").
+ qactor( smartrobot, ctxsmartrobot, "external").
+  qactor( smartrobotcaller, ctxsmartrobotcaller, "it.unibo.smartrobotcaller.Smartrobotcaller").
--- it.unibo.smartrobot2020/src/it/unibo/ctxBasicRobot/MainCtxBasicRobot.kt
+++ it.unibo.smartrobot2020/src/it/unibo/ctxBasicRobot/MainCtxBasicRobot.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 	QakContext.createContexts(
-	        "192.168.1.6", this, "smartrobot.pl", "sysRules.pl"
+	        "192.168.1.4", this, "smartrobot.pl", "sysRules.pl"
 	)
 }
 
