9e54cfcf5dc611d85138f12e4c71780e50ccec7a
anatalibo <antonio.natali@unibo.it>
2019-11-12 12:22:15 +0100

    LabRadarUsage 1

--- it.unibo.qak.radar2020/radarsys.pl
+++ it.unibo.qak.radar2020/radarsys.pl
@@ -1,7 +1,5 @@
 %====================================================================================
 % radarsys description   
 %====================================================================================
-mqttBroker("localhost", "1883").
-context(ctxradargui, "localhost",  "TCP", "8038").
- qactor( radar, ctxradargui, "it.unibo.radar.Radar").
-  qactor( demo, ctxradargui, "it.unibo.demo.Demo").
+context(ctxradar, "localhost",  "TCP", "8038").
+ qactor( radar, ctxradar, "it.unibo.radar.Radar").
