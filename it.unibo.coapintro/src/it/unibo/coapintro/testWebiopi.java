662bc9e771a5b3fe5bd8e3f397bbb18891d2c022
anatalibo <antonio.natali@unibo.it>
2019-12-29 16:41:17 +0100

    webiopi

---
+++ it.unibo.coapintro/src/it/unibo/coapintro/testWebiopi.java
@@ -0,0 +1,26 @@
+package it.unibo.coapintro;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+
+public class testWebiopi {
+private static CoapClient client = new CoapClient();	
+  	
+ 	
+ 	public static void produce(  ) {
+ 		String uriStr = "coap://192.168.43:5683/GPIO/17/value";
+  		client.setURI( uriStr );
+ 		String arg = "{ \"activationCode\"  :  \"ACTIVATION_CODE\" }";
+ 		System.out.println("USING: " + uriStr);
+ 		CoapResponse respPut = client.put(arg, MediaTypeRegistry.TEXT_PLAIN);
+ 		System.out.println("PUT RESPONSE CODE=" + respPut.getResponseText() );
+	}
+	
+	
+ 	
+	public static void main(String[] args) {
+		produce();
+   	}
+
+}
