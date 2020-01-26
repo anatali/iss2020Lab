e68a60a1f3bc11ee4a584ba0420a2f988f9e3366
anatalibo <antonio.natali@unibo.it>
2020-01-26 18:41:09 +0100

    using coap

--- it.unibo.mbot2020/node/coap/basicrobotCoap.js
+++ it.unibo.mbot2020/node/coap/basicrobotCoap.js
@@ -1,9 +1,20 @@
 var coap        = require('coap')
   , server      = coap.createServer()
+var SerialPort  = require("serialport");
+var arduinoPort = "/dev/ttyUSB0";	//COM6
+
+
+var arduinoSerialPort = new SerialPort(arduinoPort, { baudRate: 115200  });
+arduinoSerialPort.on('open',function() {
+  console.log('Serial Port ' + arduinoPort + ' is opened.');
+});
+
 
 server.on('request', function(req, res) {
   console.log( "server | url=" + req.url )
-  res.end('Hello from Coap server to robot move:' + req.url.split('/')[1] + '\n')
+  var move = req.url.split('/')[1]
+  arduinoSerialPort.write( move );
+  res.end('Hello from Coap server to robot move:' + move + '\n')
 })
 
 // the default CoAP port is 5683
@@ -40,9 +51,9 @@ function q( ){
 
 setTimeout( function(){ 
 	console.log('working ...'); 
-	w()
+	l()
 	r()
-	q( )
+	//q()
 },1000);
 
 
